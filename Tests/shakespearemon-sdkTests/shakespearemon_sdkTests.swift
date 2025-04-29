14c0aaf728f66699259a830d67ed1dd3cb850d6c
Fabrizio Scarano <scaranofbr@gmail.com>
2025-04-29 19:43:46 +0200

    Initial Commit

---
+++ Tests/shakespearemon-sdkTests/shakespearemon_sdkTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import shakespearemon_sdk
+
+final class shakespearemon_sdkTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
