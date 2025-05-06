169a2846995b47e780efb2a5df049666a35dd962
Fabrizio Scarano <scaranofbr@gmail.com>
2025-05-06 16:38:06 +0200

    feat: Documentation

--- Sources/Services/Network/HTTP/Concrete/VoidResult.swift
+++ Sources/Services/Network/HTTP/Concrete/VoidResult.swift
@@ -5,4 +5,8 @@
 //  Created by Fabrizio Scarano on 06/05/25.
 //
 
+/// Helper struct to forward HTTP calls when
+/// the expected body is empty.
+/// Not actually used in the project but added for
+/// completion's sake.
 struct VoidResult: Decodable {}
